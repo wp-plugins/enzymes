9e7edbaa09184235293ce5d55ed0f95ac6ec98c3
aercolino <aercolino@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-08 15:56:38 +0000

    Imported EnzymesOption from LoginDongleOption. Improved EnzymesOption.
    git-svn-id: https://plugins.svn.wordpress.org/enzymes/trunk@1063236 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/EnzymesOption.php
@@ -0,0 +1,125 @@
+<?php
+
+class EnzymesOption
+{
+    /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * @param string $prefix
+     */
+    public
+    function __construct( $prefix = null )
+    {
+        $this->prefix = $prefix;
+    }
+
+    /**
+     * Returns the site/user option name.
+     *
+     * @param string $username
+     *
+     * @return string
+     */
+    protected
+    function name( $username = null )
+    {
+        $result = $this->prefix . ($username
+                        ? $username
+                        : 'global_options');
+        return $result;
+    }
+
+    /**
+     * Removes the site/user option.
+     *
+     * @param string $username
+     */
+    public
+    function remove( $username = null )
+    {
+        if ( is_numeric($username) ) {
+            $user = new WP_User($username);
+            $username = $user->user_login;
+        }
+        $name = $this->name($username);
+        delete_option($name);
+    }
+
+    /**
+     * Removes site option and each user's ones.
+     */
+    public
+    function remove_all()
+    {
+        global $wpdb;
+        $wpdb->query("DELETE FROM `$wpdb->options` WHERE `option_name` LIKE '{$this->prefix}%'");
+    }
+
+    /**
+     * Retrieves the site/user option value.
+     *
+     * @param string $username
+     *
+     * @return mixed
+     */
+    public
+    function get( $username = null )
+    {
+        $name = $this->name($username);
+        $result = get_option($name, array());
+        return $result;
+    }
+
+    /**
+     * Replaces the site/user option with the given $value.
+     * The site option is set with the autoload flag to yes.
+     * The user options are set with the autoload flag to no.
+     *
+     * @param mixed  $value
+     * @param string $username
+     */
+    public
+    function set( $value, $username = null )
+    {
+        $name = $this->name($username);
+        delete_option($name);
+        $autoload = $username
+                ? 'no'
+                : 'yes';
+        add_option($name, $value, null, $autoload);
+    }
+
+    /**
+     * Retrieves the values of the given option $keys
+     *
+     * @param array  $keys
+     * @param string $username
+     *
+     * @return array
+     */
+    public
+    function keysGet( $keys, $username = null )
+    {
+        $result = array_fill_keys($keys, null);
+        $data = $this->get($username);
+        $result = array_merge($result, $data);
+        return $result;
+    }
+
+    /**
+     * Sets the given $values to their option keys
+     *
+     * @param array  $values
+     * @param string $username
+     */
+    public
+    function keysSet( $values, $username = null )
+    {
+        $data = $this->get($username);
+        $data = array_merge($data, $values);
+        $this->set($data, $username);
+    }
+}
